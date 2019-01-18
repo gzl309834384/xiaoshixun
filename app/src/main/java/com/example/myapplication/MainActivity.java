18e0094fb853c491f8d5a0f3511e05aebdf9555f
gzl <1127874346@qq.com>
2019-01-18 16:43:29 +0800

    第一次

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        hh();
+    }
+    public void hh(){
+        Toast.makeText(this, "qwe", Toast.LENGTH_SHORT).show();
     }
 }
